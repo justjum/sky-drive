338a6e95dee536ec8cb91412b1603b4096f860a6
justjum <117821093+justjum@users.noreply.github.com>
2024-08-08 10:38:48 +0930

    Being 'directory' structure model

---
+++ prisma/migrations/20240808005550_folder_tree/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "parentId" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
